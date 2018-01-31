140156f98ef00fd460f91edad144f11c2bb533ba
niufei58666 <1092586403@qq.com>
2018-01-31 08:39:32 +0800

    2018.1.30

--- js/index.js
+++ js/index.js
@@ -1,26 +1,96 @@
 window.onload=function(){
     //banner开始
-    navleft=document.querySelectorAll('.banna-left li');
-    console.log(navleft);
-    banner_list=document.querySelectorAll('.banner_list li');
+    let n=0;
+    let flag=true;
+    let bannerImg=document.querySelectorAll('.banna .banna-img a');
+    let bannerbox=document.querySelector('.banna');
+    let left=document.querySelector('.banna-xiaoyu');
+    let right=document.querySelector('.banna-dayu');
+    let circles=document.querySelectorAll('.banna-radius li');
+    function move() {
+        n++;
+        if(n>=bannerImg.length){
+            n=0;
+        }
+        bannerImg.forEach(function(val,ind) {
+            val.classList.remove('active');
+            circles[ind].classList.remove('active');
+        });
+        bannerImg[n].classList.add('active');
+        circles[n].classList.add('active');
+    }
+    let t=setInterval(move,2000);
+    bannerbox.onmouseenter=function(){
+        clearInterval(t);
+    };
+    bannerbox.onmouseleave=function(){
+        t=setInterval(move,2000);
+    };
+    right.onclick=function(){
+        if(!flag){
+            return;
+        }
+        flag=false;
+        move();
+    };
+    left.onclick=function(){
+        console.log(1)
+        if(!flag){
+            return;
+        }
+        flag=false;
+        n--;
+        if(n<0){
+            n=bannerImg.length-1;
+        }
+        bannerImg.forEach(function(val,ind) {
+            val.classList.remove('active');
+            circles[ind].classList.remove('active');
+        });
+        bannerImg[n].classList.add('active');
+        circles[n].classList.add('active');
+    };
+    circles.forEach(function(value,index) {
+        value.onclick=function () {
+            circles.forEach(function(val,ind) {
+                val.classList.remove('active');
+                bannerImg[ind].classList.remove('active');
+            });
+            this.classList.add('active');
+            bannerImg[index].classList.add('active');
+            n=index;
+        }
+    });
+    bannerImg.forEach(function(value) {
+        value.addEventListener('transitionend',function () {
+            flag=true;
+        })
+    });
 
+    navleft=document.querySelectorAll('.banna-left li');
+    banner_list=document.querySelectorAll('.banner_list li');
     navleft.forEach(function(value,index) {
         value.onmouseover=function() {
             navleft.forEach(function(a,b) {
                 banner_list[b].classList.remove('active');
+                a.classList.remove('active');
             });
+            this.classList.add('active');
             banner_list[index].classList.add('active');
         };
         value.onmouseout=function() {
             navleft.forEach(function(a,b) {
                 banner_list[b].classList.remove('active');
+                a.classList.remove('active');
             })
-        }
+        };
         banner_list[index].onmouseover=function() {
             banner_list[index].classList.add('active');
+            value.classList.add('active');
         };
         banner_list[index].onmouseout=function() {
             banner_list[index].classList.remove('active');
+            value.classList.remove('active');
         }
     });
 
@@ -66,5 +136,9 @@ window.onload=function(){
     fun(zb_a2,zb_content);
     //周边结束
 
+    //内容开始
 
+
+
+    //内容结束
 }
